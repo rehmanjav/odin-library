3819976fa6151c6e37473588dcb28b2cf38e7fcc
Rehman Javaid <rehmanjav@outlook.com>
2022-10-03 18:01:02 -0400

    Implement generateCard and updateLibrary

--- library.js
+++ library.js
@@ -36,8 +36,81 @@ function addBookToLibrary() {
 }
 
 function updateLibrary() {
-    // 
+    let cardContainer = document.querySelector(".card-container");
+    while (cardContainer.firstChild) {
+        cardContainer.removeChild(cardContainer.firstChild);
+    }
 
+    for (let i = 0; i < myLibrary.length; i++) {
+        let bookIndex = i;
+        let bookTitle = myLibrary[i]["title"];
+        let bookAuthor = myLibrary[i]["author"];
+        let bookPages = myLibrary[i]["pages"];
+        let bookRead = myLibrary[i]["read"];
+
+        let newCard = generateCard(bookTitle, bookAuthor, bookPages, bookRead, bookIndex);
+
+        cardContainer.appendChild(newCard);
+
+    }
+}
+
+function generateCard(bookTitle, bookAuthor, bookPages, bookRead, bookIndex) {
+    let card = document.createElement('div');
+    card.classList.add("card");
+    card.setAttribute("data-index", bookIndex);
+
+    // Read button div
+    let btnReadDiv = document.createElement('div');
+    btnReadDiv.classList.add("btn-read");
+
+    let btnRead = document.createElement('button');
+    if (bookRead == true) {
+        btnRead.textContent = 'READ';
+        btnRead.classList.add("btn-read-self");
+
+    } else {
+        btnRead.textContent = "UNREAD";
+        btnRead.classList.add("btn-unread-self");
+    }
+
+    btnReadDiv.appendChild(btnRead);
+
+    card.appendChild(btnReadDiv);
+
+    // Book details div
+    let bookDetailsDiv = document.createElement('div');
+    bookDetailsDiv.classList.add("book-details");
+
+    let titleP = document.createElement('p');
+    titleP.classList.add("book-title");
+    titleP.textContent = `${bookTitle}`;
+    bookDetailsDiv.appendChild(titleP);
+
+    let authorP = document.createElement('p');
+    authorP.classList.add("book-author");
+    authorP.textContent = `By: ${bookAuthor}`;
+    bookDetailsDiv.appendChild(authorP);
+
+    let pagesP = document.createElement('p');
+    pagesP.classList.add("book-pages");
+    pagesP.textContent = `Pages: ${bookPages}`;
+    bookDetailsDiv.appendChild(pagesP);
+
+    card.appendChild(bookDetailsDiv);
+
+    // Delete div
+    let btnDeleteDiv = document.createElement('div');
+    btnDeleteDiv.classList.add("btn-delete");
+
+    let btnDelete = document.createElement('button');
+    btnDelete.textContent = 'DELETE';
+
+    btnDeleteDiv.appendChild(btnDelete);
+
+    card.appendChild(btnDeleteDiv);
+
+    return card;
 }
 
 updateLibrary();
